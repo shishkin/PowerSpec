6559280cdfa050eeffb5dca3a35451dd27938f0d
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-07-20 23:40:07 +0200

    Individual results for each test

--- PowerSpec.ps1
+++ PowerSpec.ps1
@@ -1,6 +1,6 @@
 remove-module PowerSpec -ea SilentlyContinue
 import-module .\PowerSpec.psm1
 
-resolve-path ".\specs\*.ps1" | test-spec
+resolve-path ".\specs\*.ps1" | test-spec | out-null
 
 remove-module PowerSpec
