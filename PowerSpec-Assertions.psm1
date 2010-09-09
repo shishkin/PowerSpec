e68b28ecf87c00e0c8e72f5cfc795c6091ce254b
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-09-09 12:17:08 +0200

    added be_greater assertion

--- PowerSpec-Assertions.psm1
+++ PowerSpec-Assertions.psm1
@@ -140,6 +140,10 @@ function be_null ($actual) {
     return $null -eq $actual
 }
 
+function be_greater ($actual, $expected) {
+    return $actual -gt $expected
+}
+
 export-moduleMember -function `
     each,
     should,
---
+++ specs/should_be_greater.ps1
@@ -0,0 +1,5 @@
+﻿3 | should be_greater 2
+
+2 | should not be_greater 3
+
+3 | should not be_greater 3
